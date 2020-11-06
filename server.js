54a2c0b003e8fc318d3370802186c3dd31948a28
MahmoudGhnnam <mahmou999@gmail.com>
2020-11-06 23:37:14 +0200

    lab13

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const superagent = require('superagent');
 const pg = require('pg');
 const app = express();
+const methodOverride = require('method-override')
 
 app.set('view engine', 'ejs');
 
@@ -16,15 +17,13 @@ const client = new pg.Client(process.env.DATABASE_URL);
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride('_method'))
 app.use(express.static('./public'))
 
-// client.connect().then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`listening to port ${PORT}`);
-//   });
 
-// })
+
+
+
 
 
 //------------routes---------
@@ -35,13 +34,46 @@ app.get('/books/:book_id', handelSelectBook);
 
 app.post('/searches', hadnelSearch)
 
+app.post('/books', hadeladd);
+
+app.put('/books/:book_id', handelUpdate)
+
+app.delete('/books/:book_id', handelDelete)
 
 app.get('/search/new', (req, res) => {
   res.render('pages/searches/new');
 });
 
+app.get('/update/:book_id', handelupdateGET);
 
-app.post('/books', hadeladd);
+
+
+//-----------Functions---------------
+
+function handelupdateGET(req, res) {
+
+  let SQL = 'select * from books where id=$1';
+  let id = [req.params.book_id];
+  let sql = 'select DISTINCT bookshelf from books;';
+
+  console.log(id);
+  return client.query(SQL, id)
+    .then(results => {
+
+      client.query(sql).then(data => {
+        // console.log(data.rows)
+        console.log(results.rows)
+
+        res.render('pages/books/edit', { result: results.rows[0], bookshelf: data.rows })
+
+      })
+    })
+    .catch(error => {
+      close.log(error);
+      res.render('pages/error');
+    })
+
+}
 
 
 
@@ -55,7 +87,8 @@ function hadelHome(req, res) {
       console.log(results.rowCount)
       res.render('pages/index', { books: results.rows, bookCount: results.rowCount })
     })
-  // .catch(err => handleError(err, response));
+    .catch(err => handleError(err, response));
+
 
 }
 
@@ -66,15 +99,18 @@ function handelSelectBook(req, res) {
   let SQL = 'select * from books where id=$1;';
   let id = [req.params.book_id];
 
-  // console.log(id);
+  console.log(id);
   return client.query(SQL, id)
     .then(results => {
-
+       console.log(results.rows)
       res.render('pages/books/show', { result: results.rows[0] })
+    })
+    .catch(error => {
+      close.log(error);
+      res.render('pages/error');
     })
 
 }
-
 
 
 
@@ -93,11 +129,8 @@ function hadnelSearch(req, res) {
   let url = `https://www.googleapis.com/books/v1/volumes?q=+${terms}:${input}`
   console.log(url)
 
-  // console.log('qs :', input);
-  // console.log('s :', searchBy);
-  // console.log('POST request: ', req.body);
-  // console.log(req.body);
-  superagent.get(url).then(data => {
+  superagent.get(url)
+    .then(data => {
       let arr = data.body.items.map(e => {
         let newbook = new Book(e);
         return newbook;
@@ -106,7 +139,7 @@ function hadnelSearch(req, res) {
       res.render('pages/searches/show', { list: arr })
     })
     .catch(error => {
-      // console.log(error);
+      close.log(error);
       res.render('pages/error');
     })
 
@@ -121,29 +154,73 @@ function hadeladd(req, res) {
   // console.log(req.body);
   let { author, title, isbn, image_url, description, bookshelf } = req.body;
   const SQL = 'INSERT INTO books (author,title,isban,image_url,description,bookshelf) VALUES ($1, $2, $3, $4, $5, $6);';
-  let values = [author, title, isbn,  image_url, description, bookshelf];
+  let values = [author, title, isbn, image_url, description, bookshelf];
   let sql = 'select * from books;';
   let bookCount;
 
 
   client.query(sql)
     .then(results => {
-      console.log(results.rowCount)
+      // console.log(results.rowCount)
       bookCount = results.rowCount
-      console.log(bookCount)
+      // console.log(bookCount)
     })
 
-  return client.query(SQL,values)
-  .then(()=>{
+  return client.query(SQL, values)
+    .then(() => {
 
-        res.redirect(`/books/${bookCount+1}`)
-       })
+      res.redirect(`/books/${bookCount + 1}`)
+    })
+    .catch(error => {
+      close.log(error);
+      res.render('pages/error');
+    })
 
 
 }
 
 
 
+
+function handelUpdate(req, res) {
+
+  let SQL = 'update books set  author=$1 , title=$2 , isban=$3 , image_url=$4 , description=$5 , bookshelf=$6  where id=$7;';
+
+  let { author, title, isbn, image_url, description, bookshelf } = req.body;
+  let id = req.params.book_id;
+  let values = [author, title, isbn, image_url, description, bookshelf, id];
+  // console.log(values)
+
+
+  return client.query(SQL, values)
+    .then(() => {
+
+      res.redirect(`/books/${id}`)
+    })
+    .catch(error => {
+      close.log(error);
+      res.render('pages/error');
+    })
+}
+
+
+
+function handelDelete(req, res) {
+
+  let SQL = 'delete from books where id=$1;';
+  let id = [req.params.book_id];
+  console.log(id)
+
+  return client.query(SQL, id)
+    .then(() => {
+
+      res.redirect(`/`)
+    })
+    .catch(error => {
+      close.log(error);
+      res.render('pages/error');
+    })
+}
 
 
 
@@ -166,9 +243,10 @@ function Book(data) {
 
 
 //-----listening to port------
-app.listen(PORT, () => {
-  console.log(`listening to port ${PORT}`);
-});
 
+client.connect().then(() => {
+  app.listen(PORT, () => {
+    console.log(`listening to port ${PORT}`);
+  });
 
-
+})
